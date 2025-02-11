5bd839996365b5130de1bc10944ea4e25fa1a10a
McKinley <mgmorris@uab.edu>
2025-02-10 19:19:05 -0600

    added IC statement

--- group12_GA1/GA1.py
+++ group12_GA1/GA1.py
@@ -1,1 +1,8 @@
-#GA1
+# We Williams Beaumont, Hunter Forsythe, and McKinley Morris
+# declare that We have completed this
+# computer code in accordance with the UAB Academic Integrity
+# Code and the UAB CS Honor Code. I/We have read the UAB
+# Academic Integrity Code and understand that any breach of the
+# Code may result in severe penalties.
+# Student signature(s)/initials: MM
+# Date: 2/23/2025
