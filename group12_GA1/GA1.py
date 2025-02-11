7f6c1d04818d17f22ec8af7ea925c33d3a38c5cb
McKinley <mgmorris@uab.edu>
2025-02-11 11:42:02 -0600

    function markup

--- group12_GA1/GA1.py
+++ group12_GA1/GA1.py
@@ -6,3 +6,25 @@
 # Code may result in severe penalties.
 # Student signature(s)/initials: MM
 # Date: 2/23/2025
+
+def c_ciphertext(text):
+    for i in range(len(text)):
+        print("user text")
+        
+        
+    
+def choice_switch(user_choice):
+    match user_choice:
+        case 1:
+            user_input = input("Type your message to be encrypted\n")
+        case 2:
+            user_input = input("Choose your file to be encrypted\n")
+        case default:
+            print("Invalid input")
+
+
+try:
+    user_choice = int(input("Enter 1 for typed input\nEnter 2 to select txt file\n"))
+    choice_switch(user_choice)
+except ValueError:
+    print("Please enter a valid option")
