f3f4fdbff18a6f97078b7b62e37634f9b9fbd2e2
Williams Beaumont <williams.a.beaumont@gmail.com>
2025-02-23 15:17:43 -0600

    Revised the code and moved relevant files into the GA1 directory.

--- group12_GA1/GA1.py
+++ group12_GA1/GA1.py
@@ -4,7 +4,7 @@
 # Code and the UAB CS Honor Code. I/We have read the UAB
 # Academic Integrity Code and understand that any breach of the
 # Code may result in severe penalties.
-# Student signature(s)/initials: MM, HF
+# Student signature(s)/initials: MM, HF, WAB
 # Date: 2/23/2025
 
 #Caesar cipher encryption
@@ -12,8 +12,8 @@ def c_encrypt(plain_text, key):
     cipher_text = ""
     for i in range(len(plain_text)):
         P = ord(plain_text[i]) #P = ASCII code of plaintext char
-        if 0x21 <= P <= 0x7E: #if char is within printable range
-            C = ( (P - 32 + int(key)) % 96) + 32 #C = ASCII code of ciphertext char
+        if 0x20 <= P <= 0xFF: #if char is within printable range
+            C = ( (P - 32 + int(key)) % 224) + 32 #C = ASCII code of ciphertext char
             cipher_char = chr(C) #ASCII code C -> char
         else:
             cipher_char = plain_text[i] #copy without change
@@ -26,8 +26,8 @@ def c_decrypt(cipher_text, key):
     plain_text = ""
     for i in range(len(cipher_text)):
         C = ord(cipher_text[i]) #C = ASCII code of cipher char
-        if 0x21 <= C <= 0x7E: #if char is within printable range
-            P = ( (C - 32 - int(key)) % 96) + 32 #P = ASCII code of plaintext char
+        if 0x20 <= C <= 0xFF: #if char is within printable range
+            P = ( (C - 32 - int(key)) % 224) + 32 #P = ASCII code of plaintext char
             plain_char = chr(P) #ASCII code P -> char
         else:
             plain_char = cipher_text[i] #copy without change
@@ -35,21 +35,23 @@ def c_decrypt(cipher_text, key):
     return plain_text
         
 
-file_path = input("Type the name of your file\n")
+file_path = input("Enter the name of target file:\n")
 file = open(file_path, "r")
 content = file.read()
-key = input("Enter your key (integer)\n")
-user_choice = int(input("Enter 1 for encryption\nEnter 2 for decryption\n"))
+key = input("Enter an integer key from 0 to 223:\n")
+user_choice = int(input("Enter 1 for encryption or 2 for decryption:\n"))
 try:
     match user_choice:
         case 1:
             cipher_text = c_encrypt(content, key)
             with open("cipher_text.txt", "w") as cipher_file:
                     cipher_file.write(cipher_text)
+            print("Your file has been encrypted to cipher_text.txt")
         case 2:
             plain_text = c_decrypt(content, key)
             with open("plain_text.txt", "w") as plain_file:
                     plain_file.write(plain_text)
+            print("Your file has been decrypted to plain_text.txt")
         case default:
             print("Invalid input")
 except ValueError:
