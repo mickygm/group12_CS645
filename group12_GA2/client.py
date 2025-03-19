b754c326160769420786a745e444a23fee80bc6d
micky <mckinley628@gmail.com>
2025-03-19 15:20:26 -0500

    parts 1-3 done

--- group12_GA2/client.py
+++ group12_GA2/client.py
@@ -1,13 +1,32 @@
 import socket
+import ascon
 
-host = input("Type the User A's IP address: ")  
-port = 12345        
+IP = input("Enter server IP: ")
+port = 12345
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as c:
-    c.connect((host, port))
-    print("Connected to User A.")
+#SHARED SECRETS
+KEY = b'SECRETKEY1234567'
+NONCE = b"\x00" * 16
+ASSOCIATED_DATA = b"CS645/745 Modern Cryptography: Secure Messaging"
+
+def encrypt_message(plaintext):
+    return ascon.encrypt(key=KEY, nonce=NONCE, associateddata=ASSOCIATED_DATA,
+                        plaintext=plaintext.encode(), variant="Ascon-128")
+
+def decrypt_message(ciphertext):
+    return ascon.decrypt(key=KEY, nonce=NONCE, associateddata=ASSOCIATED_DATA, 
+                        ciphertext=ciphertext, variant="Ascon-128").decode()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+    client_socket.connect((IP, port))
+    print("Connected to User B.")
     while True:
-        message = c.recv(1024).decode()  
-        print(f"User A: {message}")
-        reply = input("User B: ")  
-        c.sendall(reply.encode())  
+        message = input("User A: ")
+        client_socket.sendall(encrypt_message(message))
+        if message.lower() == 'exit':
+            break
+        encrypted_msg = client_socket.recv(1024)
+        decrypted_message = decrypt_message(encrypted_msg)
+        print("User B: ", decrypted_message)
+
+print("Connection ended")
--- group12_GA2/server.py
+++ group12_GA2/server.py
@@ -1,17 +1,35 @@
 import socket
+import ascon
 
-host = '0.0.0.0'
+IP = '' #WILL LISTEN TO ANY DEVICE ON THE NETWORK
 port = 12345
 
+#SHARED SECRETS
+KEY = b'SECRETKEY1234567'
+NONCE = b"\x00" * 16
+ASSOCIATED_DATA = b"CS645/745 Modern Cryptography: Secure Messaging"
+
+def encrypt_message(plaintext):
+    return ascon.encrypt(key=KEY, nonce=NONCE, associateddata=ASSOCIATED_DATA,
+                        plaintext=plaintext.encode(), variant="Ascon-128")
+
+def decrypt_message(ciphertext):
+        return ascon.decrypt(key=KEY, nonce=NONCE, associateddata=ASSOCIATED_DATA,
+                            ciphertext=ciphertext, variant="Ascon-128").decode()
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((host, port))
+    s.bind((IP, port))
     s.listen()
-    print("Waiting for a connection...")
+    print("Connecting...")
     conn, addr = s.accept()
-    with conn:
-        print(f"Connected to User B: {addr}")
-        while True:
-            message = input("User A: ")  
-            conn.sendall(message.encode())  
-            reply = conn.recv(1024).decode()  
-            print(f"User B: {reply}")
+    print("Connected to User A:", addr)
+    while True:
+        encrypted_msg = conn.recv(1024)
+        decrypted_msg = decrypt_message(encrypted_msg)
+        print("User A:", decrypted_msg)
+        message = input("User B: ")
+        conn.sendall(encrypt_message(message))
+        if message.lower() == 'exit':
+            break
+
+print("Connection ended")
