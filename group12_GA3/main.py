3590d30ae1d813c310206d1b5d23895f996c3214
Williams Beaumont <williams.a.beaumont@gmail.com>
2025-04-19 20:26:36 -0500

    added scheduling, snapshots, and hashing with previous results

--- group12_GA3/main.py
+++ group12_GA3/main.py
@@ -1,20 +1,36 @@
+# We Williams Beaumont, Hunter Forsythe, and McKinley Morris
+# declare that We have completed this
+# computer code in accordance with the UAB Academic Integrity
+# Code and the UAB CS Honor Code. I/We have read the UAB
+# Academic Integrity Code and understand that any breach of the
+# Code may result in severe penalties.
+# Student signature(s)/initials: WAB, MM, HF
+# Date: 4/19/2025
+
 import os
 from cryptography.hazmat.primitives import hashes
+import shutil
+import schedule
+import time
 
-#SHA-256 fx
+# Performs SHA-256 hash function
 def sha256(data):
     h = hashes.Hash(hashes.SHA256())
     h.update(data)
     return h.finalize()
 
+# Computes a Merkle hash from a list of files
 def merkle_tree(leaves):
     #IF FOLDER IS EMPTY, RETURN HASH OF EMPTY STRING
     if not leaves:
+        #print("\nEmpty directory.")
         return sha256(b'')
     #HASH ALL LEAVES
     nodes = [sha256(leaf) for leaf in leaves]
+    #print("\nList of all node hashes: ", nodes)
     #COMBINE NODES UNTIL ROOT IS REACHED
     while len(nodes) > 1:
+        level = 0
         next_level = []
         for i in range(0, len(nodes), 2): #combining hashes of every two nodes
             left = nodes[i]
@@ -23,9 +39,12 @@ def merkle_tree(leaves):
             combined = sha256(left + right)
             next_level.append(combined)
         nodes = next_level
+        level = level + 1
+        #print("\nList of combined hashes for level ", level, nodes)
     #RETURN MERKLE ROOT
     return nodes[0]
 
+# Creates a list of all files in a directory and passes that list to merkle_tree
 def merkle_root_from_directory(path):
     leaves = []
     for root, _, files in os.walk(path):
@@ -37,6 +56,45 @@ def merkle_root_from_directory(path):
                 print(f"Error reading {f}: {e}")
     return merkle_tree(leaves)
 
+# Creates a snapshot of the directory and hashes the digest of that directory with the previous snapshot's digest
+def merkleSnap(dir, snap):
+    global prevHash
+    global snapIteration
+    shutil.copytree(dir, snap, dirs_exist_ok=True)
+    root = merkle_root_from_directory(snap)
+    newHash = sha256(prevHash + root)
+    prevHash = newHash
+    print(f"\nHash of Snapshot {snapIteration}:", newHash.hex())
+    snapIteration = snapIteration + 1
+
+# Initializes first hash value to all 0s
+prevHash = b"\x00" * 256
+
+# Initializes the snapshot number
+snapIteration = 1
+
+# Main function
 if __name__ == "__main__":
-    root = merkle_root_from_directory("group12_GA3/Test Root")
-    print("Merkle Root:", root.hex())
+    # Gets information from the user on how they want to use the program
+    dirName = input("\nEnter directory to hash: ")
+    snapName = input("\nEnter name for snapshot directory: ")
+    freq = int(input("\nChoose how often to take a snapshot:\n1. Every 30 seconds\n2. Every minute\n3. Every hour\n4. Every 24 hours\n"))
+    goodInput = False
+    while not goodInput: 
+        if 1 <= freq <= 4: 
+            goodInput = True
+        else:
+            key = int(input("\nInvalid selection. Please enter a valid selection: "))
+    # Schedules the program to run based on the user's selection
+    if freq == 1:
+        schedule.every(30).seconds.do(merkleSnap, dir = dirName, snap = snapName)
+    elif freq == 2:
+        schedule.every().minute.do(merkleSnap, dir = dirName, snap = snapName)
+    elif freq == 3:
+        schedule.every().hour.do(merkleSnap, dir = dirName, snap = snapName)
+    else:
+        schedule.every(24).hours.do(merkleSnap, dir = dirName, snap = snapName)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+    
